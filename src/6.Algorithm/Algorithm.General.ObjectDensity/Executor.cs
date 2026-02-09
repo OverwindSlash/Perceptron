1f999d81fce8314be0119e1b828eedb0487b8447
Overwind <overwind@outlook.com>
2026-02-09 17:55:30 +0800

    feat: 添加通用对象密度算法项目基础结构
    - 创建 Algorithm.General.ObjectDensity 项目文件并配置项目引用
    - 添加解决方案文件中的项目引用条目
    - 实现算法执行器基础类，包含算法名称、版本和描述信息

---
+++ src/6.Algorithm/Algorithm.General.ObjectDensity/Executor.cs
@@ -0,0 +1,24 @@
+﻿using Algorithm.Common;
+using Perceptron.Domain.Entity.Pipeline;
+using Perceptron.Domain.Entity.VideoStream;
+using Perceptron.Service.Pipeline;
+
+namespace Algorithm.General.ObjectDensity;
+
+public class Executor : AlgorithmBase
+{
+    public Executor(AnalysisPipeline pipeline, Dictionary<string, string> preferences) 
+        : base(pipeline, preferences)
+    {
+        _pipeline = pipeline;
+
+        AlgorithmName = "Object Density";
+        AlgorithmVersion = "1.0.0";
+        AlgorithmDescription = "Detects object density in video frames.";
+    }
+
+    public override AnalysisResult Analyze(Frame frame)
+    {
+        throw new NotImplementedException();
+    }
+}
